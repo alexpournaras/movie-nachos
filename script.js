facde493808ba0ea7112c1821664d518e2aee524
Alexandros Pournaras <115292358+alexpournaras@users.noreply.github.com>
2023-01-02 20:15:38 +0200

    Initial Commit

---
+++ script.js
@@ -0,0 +1,67 @@
+window.onload = function () {
+
+	const burger_menu = document.getElementById('burger-menu');
+	const menu_links = document.getElementById('menu-links');
+	const icon = burger_menu.querySelector('i');
+
+	burger_menu.addEventListener('click', function () {
+		menu_links.classList.toggle("show-menu");
+		icon.classList.toggle('fa-times');
+		icon.classList.toggle('fa-bars');
+
+
+	});
+
+	setSlide(1);
+};
+
+var active_slide = null;
+var auto_scroll = false;
+
+var intervalId = setInterval(() => {
+	if (auto_scroll) nextSlide();
+}, 5000);
+
+function nextSlide() {
+	active_slide += 1;
+	if (active_slide > 3) active_slide = 1;
+	setSlide(active_slide);
+}
+
+function previousSlide() {
+	active_slide -= 1;
+	if (active_slide < 1) active_slide = 3;
+	setSlide(active_slide);
+}
+
+function changeSlide(slide) {
+	active_slide = slide;
+	setSlide(slide);
+}
+
+function setSlide(slide) {
+	if (!active_slide) active_slide = slide;
+
+	clearInterval(intervalId);
+
+	intervalId = setInterval(() => {
+		if (auto_scroll) nextSlide();
+	}, 5000);
+
+	var i;
+	var slides = document.getElementsByClassName("Containers");
+	var circles = document.getElementsByClassName("dots");
+	if (slide > slides.length) { slide = 1 }
+	if (slide < 1) { slide = slides.length }
+
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+
+	for (i = 0; i < circles.length; i++) {
+		circles[i].className = circles[i].className.replace(" enable", "");
+	}
+
+	slides[slide - 1].style.display = "block";
+	circles[slide - 1].className += " enable";
+}
