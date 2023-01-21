4098f786a6612a0fc01b97ef70a9568a7f7724d8
Alexandros Pournaras <apournaras@york.citycollege.eu>
2023-01-21 14:56:31 +0200

    Seperate filters script to fix console errors on other pages

--- script.js
+++ script.js
@@ -43,42 +43,4 @@ document.addEventListener('DOMContentLoaded', function() {
 	messageSendBtn.addEventListener('click', function () {
 		messageSentConfirmation.style.display = "block";
 	});
-
-	// Toggle filters dropdown
-	const filterSortBtn = document.getElementById('filter-sort');
-	const filterGenreBtn = document.getElementById('filter-genre');
-	const filterYearBtn = document.getElementById('filter-year');
-	const filterSortDropdown = document.getElementById('filter-sort-dropdown');
-	const filterGenreDropdown = document.getElementById('filter-genre-dropdown');
-	const filterYearDropdown = document.getElementById('filter-year-dropdown');
-
-	filterSortBtn.addEventListener('click', function (e) {
-		e.preventDefault()
-		e.stopPropagation();
-		filterSortDropdown.style.display = "block";
-		filterGenreDropdown.style.display = "none";
-		filterYearDropdown.style.display = "none";
-	});
-
-	filterGenreBtn.addEventListener('click', function (e) {
-		e.preventDefault()
-		e.stopPropagation();
-		filterGenreDropdown.style.display = "block";
-		filterSortDropdown.style.display = "none";
-		filterYearDropdown.style.display = "none";
-	});
-
-	filterYearBtn.addEventListener('click', function (e) {
-		e.preventDefault()
-		e.stopPropagation();
-		filterYearDropdown.style.display = "block";
-		filterSortDropdown.style.display = "none";
-		filterGenreDropdown.style.display = "none";
-	});
-
-	document.addEventListener('click', function (e) {
-		if (!filterSortDropdown.contains(e.target)) filterSortDropdown.style.display = "none";
-		if (!filterGenreDropdown.contains(e.target)) filterGenreDropdown.style.display = "none";
-		if (!filterYearDropdown.contains(e.target)) filterYearDropdown.style.display = "none";
-	});
 });
